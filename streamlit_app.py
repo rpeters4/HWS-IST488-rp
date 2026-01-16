86ad9d00a8aa56f6b7a43202d3d6b51d5af3c1b5
Ryan Peters <petersryan1993@gmail.com>
2026-01-16 18:54:41 +0000

    Made changes to handle pdfs and a pdf helpder function. Next steps are to add a model switcher between all GPT models, and to add a way to file ext detection with conditional reading logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,22 @@
+
 import streamlit as st
 from openai import OpenAI
+import fitz
+
+
+#helper function for reading these PDFs
+def read_pdf(pdf_file):
+    pdf_contents = pdf_file.read()
+    document = fitz.open(stream=pdf_contents, filetype="pdf")
+    text = "" #empty buffer
+    for page in document:
+        text += page.get_text()
+    document.close()
+    return text
+#---------------------------------------
 
 # Show title and description.
-st.title("📄 Document question answering")
+st.title("MY (Ryan) Document question answering")
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
@@ -19,35 +33,43 @@ else:
     # Create an OpenAI client.
     client = OpenAI(api_key=openai_api_key)
 
-    # Let the user upload a file via `st.file_uploader`.
-    uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
-    )
+    try:
 
-    # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
-        disabled=not uploaded_file,
-    )
+        client.models.list()
+        st.success("API Key valid, proceed...")
 
-    if uploaded_file and question:
-
-        # Process the uploaded file and question.
-        document = uploaded_file.read().decode()
-        messages = [
-            {
-                "role": "user",
-                "content": f"Here's a document: {document} \n\n---\n\n {question}",
-            }
-        ]
-
-        # Generate an answer using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            stream=True,
+        # Let the user upload a file via `st.file_uploader`.
+        uploaded_file = st.file_uploader(
+            "Upload a document (.txt or .pdf)", type=("txt", "pdf")
         )
 
-        # Stream the response to the app using `st.write_stream`.
-        st.write_stream(stream)
+        # Ask the user for a question via `st.text_area`.
+        question = st.text_area(
+            "Now ask a question about the document!",
+            placeholder="Can you give me a short summary?",
+            disabled=not uploaded_file,
+        )
+
+        #change below to allow text and pdf, pdf doesn't appear to be handled by this. 
+        if uploaded_file and question:
+
+            # Process the uploaded file and question.
+            document = uploaded_file.read().decode()
+            messages = [
+                {
+                    "role": "user",
+                    "content": f"Here's a document: {document} \n\n---\n\n {question}",
+                }
+            ]
+
+            # Generate an answer using the OpenAI API.
+            stream = client.chat.completions.create(
+                model="gpt-3.5-turbo", ##might need to be changed with new formatting for adding a model selector. 
+                messages=messages,
+                stream=True,
+            )
+
+            # Stream the response to the app using `st.write_stream`.
+            st.write_stream(stream)
+    except Exception as e:
+        st.error("Invalid API Key, please try again.") 
