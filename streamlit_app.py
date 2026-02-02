5aa525c4c477b0fe87a4f07c85cb5586ab672913
Ryan Peters <petersryan1993@gmail.com>
2026-02-02 09:24:52 -0500

    changed default hw

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 # Define pages
-hw1 = st.Page("HW/Homework1.py", title="Homework 1", icon=":material/description:", default=True)
-hw2 = st.Page("HW/HW2.py", title="Homework 2", icon=":material/description:")
+hw1 = st.Page("HW/Homework1.py", title="Homework 1", icon=":material/description:")
+hw2 = st.Page("HW/HW2.py", title="Homework 2", icon=":material/description:", default=True)
 
 # Create navigation
 pg = st.navigation([hw1, hw2])
@@ -10,5 +10,5 @@ pg = st.navigation([hw1, hw2])
 # Configure page
 st.set_page_config(page_title="HW Manager", page_icon=":material/school:")
 
-# Run the selected page
+# Run the selected pagea
 pg.run()
